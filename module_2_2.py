de21336a0b4a51f4cbdfcb3f9344aebe698ca999
Vladimir <SVova91@mail.ru>
2024-11-21 01:57:17 +0300

    Модуль_2
    Условная конструкция. Операторы if, elif, else

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+first = input('Введите число: ')
+second = input('Введите число: ')
+third = input('Введите число: ')
+if first == third != second:
+    print('Одинаковых чисел: 2')
+elif first == second == third:
+    print('Одинаковых чисел: 3')
+elif first != second != third:
+    print('Одинаковых чисел: 0')
+else:
+    print('Одинаковых чисел: 2')
