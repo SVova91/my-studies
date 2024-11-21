8253acb332a94cc7dc830eea2134ae5ecf62f9aa
Vladimir <SVova91@mail.ru>
2024-11-21 19:25:38 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,11 +1,14 @@
 first = input('Введите число: ')
 second = input('Введите число: ')
 third = input('Введите число: ')
-if first == third != second:
-    print('Одинаковых чисел: 2')
-elif first == second == third:
+if first == second == third:
     print('Одинаковых чисел: 3')
-elif first != second != third:
-    print('Одинаковых чисел: 0')
-else:
+elif first == third != second:
     print('Одинаковых чисел: 2')
+elif third == second != first:
+    print('Одинаковых чисел: 2')
+elif first == second != third:
+    print('Одинаковых чисел: 2')
+else:
+    print('Одинаковых чисел: 0')
+    
