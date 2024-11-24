7cb2df838f187f46daf06b94ec9a3da72f81a38b
Vladimir <SVova91@mail.ru>
2024-11-24 23:01:43 +0300

    Add files via upload
    Цикл For

---
+++ module_2_4.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for num in numbers:
+    is_prime = True
+    if num < 2:
+        is_prime = False
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+    if is_prime:
+        primes.append(num)
+    else:
+        not_primes.append(num)
+not_primes.remove(1)  # Не знаю, как единицу по другому убрать....
+print('Primes:', primes)
+print('Not_primes:', not_primes)
