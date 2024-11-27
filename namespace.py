779b9bcebde465cc7b4dec5519c4ca5f9937ddd4
Vladimir <SVova91@mail.ru>
2024-11-27 23:08:43 +0300

    Д/з, пространство имён

---
+++ namespace.py
@@ -0,0 +1,24 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string_lower = string.lower()
+    list_lower = [s.lower() for s in list_to_search]
+    if string_lower in list_lower:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
